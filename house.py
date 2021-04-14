a307047621cbbaed86e36906b075c5090167c74b
ALee008 <39236016+ALee008@users.noreply.github.com>
2021-04-14 10:45:50 +0200

    Add files via upload
    added coloring

--- house.py
+++ house.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 
 def gehe_zum_startpunkt(x, y):
@@ -14,24 +15,30 @@ def gehe_zum_startpunkt(x, y):
     return None
 
 
-def male_quadrat(laenge: float):
+def male_quadrat(laenge: float, farbe: str ="white"):
     """Zeichne ein Quadrat mit den Seitenlaengen `laenge`.
 
     :param laenge: die Seitenlaenge des Quadrates
+    :param farbe: die Farbe des Quadrates
     """
+    stift.fillcolor(farbe)
+    stift.begin_fill()
     for _ in range(4):
         stift.forward(laenge)
         stift.left(90)
-
+    stift.end_fill()
     return None
 
 
-def male_rechteck(breite: float, hoehe: float):
+def male_rechteck(breite: float, hoehe: float, farbe: str = "white"):
     """Zeichne ein Rechteck mit den Seitenlaengen `breite` und `hoehe`.
 
     :param breite: die Breite des Rechtecks
     :param hoehe: die Hoehe des Rechtecks
+    :param farbe: Farbe des Rechtecks
     """
+    stift.fillcolor(farbe)
+    stift.begin_fill()
     for i in range(4):
         if i % 2 == 0:
             stift.forward(breite)
@@ -39,55 +46,58 @@ def male_rechteck(breite: float, hoehe: float):
         else:
             stift.forward(hoehe)
             stift.left(90)
-
+    stift.end_fill()
     return None
 
 
-def male_tuer(breite: float, hoehe: float):
+def male_tuer(breite: float, hoehe: float, farbe: str = "white"):
     """Male die Tuer. Es wird angenommen, dass der Startpunkt die untere linke Ecke des Hauses ist.
 
     :param breite: die Breite der Tuer
     :param hoehe: die Hoehe der Tuer
+    :param farbe: die gewuenschte Farbe fuer die Tuer
     """
     stift.setheading(0.0)
     stift.forward(50)
-    male_rechteck(breite, hoehe)
+    male_rechteck(breite, hoehe, farbe)
 
     return None
 
 
-def male_dach(dach_hoehe: float):
+def male_dach(dach_hoehe: float, farbe: str ="white"):
     """Male das Dach, abhaengig von der Breite und Hoehe des Hauses.
 
-    :param haus_breite: die y-Koordinate der Startposition des Hauses
-    :param haus_hoehe: die x-Koordinate der Startposition des Hauses
     :param dach_hoehe: die gewuenschte Dachhoehe
+    :param farbe: gewuenschte Farbe fuer das Dach
     """
     mitte_des_daches = START_PUNKT_X + HAUS_BREITE / 2, -START_PUNKT_Y + dach_hoehe
     stift.up()
     stift.goto(START_PUNKT_X, START_PUNKT_Y + HAUS_HOEHE)
     stift.down()
+    stift.fillcolor(farbe)
+    stift.begin_fill()
     stift.goto(mitte_des_daches)
     stift.goto(START_PUNKT_X + HAUS_BREITE, START_PUNKT_Y + HAUS_HOEHE)
-
+    stift.end_fill()
     return None
 
 
-def male_fenster(laenge: float):
+def male_fenster(laenge: float, farbe: str ="white"):
     """Male zwei Fenster ins Haus. Beide haben die selbe Groesse.
 
     :param laenge: Fenstergroesse
+    :param farbe: die gewuenschte Fensterfarbe
     """
     stift.up()
     linkes_fenster_x, linkes_fenster_y = START_PUNKT_X + 0.1 * HAUS_BREITE, START_PUNKT_Y + 0.75 * HAUS_HOEHE
     stift.goto(linkes_fenster_x, linkes_fenster_y)
     stift.down()
-    male_quadrat(laenge)
+    male_quadrat(laenge, farbe)
     stift.up()
     rechtes_fenster_x, rechtes_fenster_y = -linkes_fenster_x - 0.1 * HAUS_BREITE, linkes_fenster_y
     stift.goto(rechtes_fenster_x, rechtes_fenster_y)
     stift.down()
-    male_quadrat(laenge)
+    male_quadrat(laenge, farbe)
 
     return None
 
@@ -95,13 +105,13 @@ def male_fenster(laenge: float):
 if __name__ == '__main__':
     stift = turtle.Turtle("turtle")
     stift.getscreen()
-    stift.speed("slow")
+    stift.speed("fast")
     START_PUNKT_X, START_PUNKT_Y = -200, -200
     HAUS_BREITE, HAUS_HOEHE = 400, 350
     gehe_zum_startpunkt(START_PUNKT_X, START_PUNKT_Y)
     # ab hier faengt die Zeichnung an
     male_rechteck(HAUS_BREITE, HAUS_HOEHE)
-    male_tuer(100, 200)
-    male_dach(dach_hoehe=80)
-    male_fenster(40)
-    print()
+    male_tuer(100, 200, "brown")
+    male_dach(dach_hoehe=80, farbe="red")
+    male_fenster(40, "yellow")
+    time.sleep(1)
